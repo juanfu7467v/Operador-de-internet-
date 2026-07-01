39ff4aa357c1c9a0527ec39b5cb9ae0256f2755d
juanfu7467v <movistarcervise@gmail.com>
2026-06-30 19:27:25 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,16 +4,9 @@
 sed -i "s/8080/${PORT}/g" /etc/xray/config.json
 
 echo "========================================================="
-echo " Servidor de pruebas iniciado con éxito en Fly.io "
-echo " Alerta: Se destruirá automáticamente en 24 horas exactas "
+echo " Servidor iniciado correctamente en Fly.io "
+echo " El servicio permanecerá activo mientras la instancia siga en ejecución "
 echo "========================================================="
-
-# Temporizador en segundo plano (86400 segundos = 24 horas)
-(
-    sleep 86400
-    echo "[!] Límite de 24 horas alcanzado. Ejecutando autodestrucción del servicio..."
-    kill -15 1
-) &
 
 # Inicia el motor de red en primer plano
 exec /usr/local/bin/xray run -c /etc/xray/config.json
