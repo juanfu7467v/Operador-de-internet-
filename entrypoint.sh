8d48287a1d40b7658ffb330ed95232e3cc410214
juanfu7467v <movistarcervise@gmail.com>
2026-07-01 21:51:12 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
-# Modifica el puerto de escucha dinámicamente según lo requiera Fly.io
-sed -i "s/8080/${PORT}/g" /etc/xray/config.json
-
 echo "========================================================="
 echo " Servidor iniciado correctamente en Fly.io "
-echo " El servicio permanecerá activo mientras la instancia siga en ejecución "
 echo "========================================================="
 
-# Inicia el motor de red en primer plano
+# Inicia el motor directamente respetando el puerto 8080 del config.json
 exec /usr/local/bin/xray run -c /etc/xray/config.json
